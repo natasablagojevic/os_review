b571c439e19c900d63d7be59989552ec9a188e13
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-14 11:34:29 +0100

    obilazak direktorijuma

--- ispiti/2022/sep1/1.c
+++ ispiti/2022/sep1/1.c
@@ -27,8 +27,48 @@ void greska(const char *msg)
     exit(EXIT_FAILURE);
 }
 
-int main(int argc, char **argv)
+void get_day(int dan)
 {
+    switch (dan) {
+        case 0 : printf("nedelja\n"); break;
+        case 1 : printf("ponedeljak\n"); break;
+        case 2 : printf("utorak\n"); break;
+        case 3 : printf("sreda\n"); break;
+        case 4 : printf("cetvrtak\n"); break;
+        case 5 : printf("petak\n"); break;
+        case 6 : printf("subota\n"); break;
+    } 
+}
+
+int f(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+    if (typeflag == FTW_F) {
+        struct tm *vreme = localtime(&(sb->st_mtime));
+            if (vreme == NULL)
+                greska("localtime failed");
+
+        printf("%s ", fpath + ftwbuf->base);
+        get_day(vreme->tm_wday);
+    }
+
+    return 0;
+}
+
+// a.out dir
+int main(int argc, char **argv)
+{   
+    if (argc != 2)
+        greska("args failed");
+
+    struct stat sb;
+    if (stat(argv[1], &sb) == -1)
+        greska("stat failed");
+
+    if (!S_ISDIR(sb.st_mode))
+        greska("nije dir");
+
+    if (nftw(argv[1], f, 50, 0) == -1)
+        greska("nije dir");
 
 
 
