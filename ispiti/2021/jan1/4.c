beb0b07e38b4ee4d0d2bab6202918f685fc242fd
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-14 12:37:35 +0100

    obilazak direktorijuma

--- ispiti/2021/jan1/4.c
+++ ispiti/2021/jan1/4.c
@@ -21,16 +21,34 @@
 #define RD_END 0
 #define WR_END 1
 
+int p, k;
+char *ekst;
+int brojac = 0;
+
 void greska(const char *msg)
 {
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
-// a.out dir p k
+int f(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+    if (typeflag == FTW_F) {
+        char *ekstenzija = strrchr(fpath + ftwbuf->base, '.');
+            if (ekstenzija == NULL)
+                greska("strrchr failed");
+
+        if (strcmp(ekst, ekstenzija) == 0 && ftwbuf->level >= p && ftwbuf->level <= k)
+            brojac++;
+    }
+
+    return 0;
+}
+
+// a.out dir ekst p k
 int main(int argc, char **argv)
 {
-    if (argc != 4)
+    if (argc != 5)
         greska("args failed");
 
     struct stat sb;
@@ -40,8 +58,20 @@ int main(int argc, char **argv)
     if (!S_ISDIR(sb.st_mode))
         greska("nije dir");
 
-    
+    p = atoi(argv[3]);
+    k = atoi(argv[4]);
 
+    ekst = malloc(strlen(argv[2]) + 1);
+        if (ekst == NULL)
+            greska("ekst malloc failed");
 
+    strcpy(ekst, argv[2]);
+
+    if (nftw(argv[1], f, 50, 0) == -1)
+        greska("nftw failed");
+
+    printf("%d\n", brojac);
+
+    free(ekst);
     exit(EXIT_SUCCESS);
 }
