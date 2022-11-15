5357837a740a52d62c59e7b4944006426393ba8f
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-15 12:58:17 +0100

    niti

---
+++ ispiti/2021/jan2/2.c
@@ -0,0 +1,38 @@
+#define _XOPEN_SOURCE 700
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<ftw.h>
+#include<semaphore.h>
+#include<sys/mman.h>
+#include<sys/wait.h>
+#include<pthread.h>
+#include<time.h>
+#include<string.h>
+#include<math.h>
+#include<ctype.h>
+#include<stdbool.h>
+
+#define UNUSED(X) (void)(X)
+#define RD_END 0
+#define WR_END 1
+
+void greska(const char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void 
+
+int main(int argc, char **argv)
+{
+
+
+
+    exit(EXIT_SUCCESS);
+}
