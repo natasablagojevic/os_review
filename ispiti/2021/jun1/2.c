e6e8e3d4693ff329b00706c1fdbe2b2cb7678994
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-18 12:56:12 +0100

    proces success

--- ispiti/2021/jun1/2.c
+++ ispiti/2021/jun1/2.c
@@ -27,11 +27,86 @@ void greska(const char *msg)
     exit(EXIT_FAILURE);
 }
 
-
+// a.out putanja
 int main(int argc, char **argv)
-{
+{   
+    if (argc != 2)
+        greska("args failed");
+
+    int cld2Par[2];
+    if (pipe(cld2Par) == -1)
+        greska("pipe failed");
+
+    pid_t childPid = fork();
+        if (childPid == -1)
+            greska("fork failed");
+
+    if (childPid > 0) {
+        /* PARENT */
+        close(cld2Par[WR_END]);
+
+        FILE *f = fdopen(cld2Par[RD_END], "r");
+            if (f == NULL)
+                greska("fdopen failed");
+
+        char *linija = NULL;
+        size_t linijalen = 0;
+        char pom[1024];
+        char pompom[1024];
+
+        while (getline(&linija, &linijalen, f) != -1) {
+            // printf("%s", linija);
+
+            sscanf(linija, "%s%s", pom, pompom);
+
+            // printf("%s %s\n", pom, pompom);
+
+            if (strcmp(pom, "Access:") == 0)
+                break;
+        }
+
+        // printf("------------\n");
+        //printf("%s %s\n", pom, pompom);
+
+        int n = strlen(pompom);
+        int k = 0;
+        char result[1024];
+
+        for (int i = 6; i < n-1; i++)
+            result[k++] = pompom[i];
+
+        result[k] = 0;
+        printf("%s\n", result);
 
 
+        close(cld2Par[WR_END]);
+    } else {
+        /* CHILD */
+        close(cld2Par[RD_END]);
+        
+        if (dup2(cld2Par[WR_END], STDOUT_FILENO) == -1)
+            greska("dup2 failed");
 
+        if (execlp("stat", "stat", argv[1], NULL) == -1)
+            greska("exelp failed");
+
+        close(cld2Par[WR_END]);
+        exit(EXIT_FAILURE);
+    }   
+
+    int status = 0;
+    if (wait(&status) == -1)
+        greska("wait failed");
+
+    if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
+        fprintf(stderr, "uspeh\n");
+    else 
+        fprintf(stdout, "neuspeh\n");
+
+    
+
+
+    close(cld2Par[RD_END]);
+    close(cld2Par[WR_END]);
     exit(EXIT_SUCCESS);
 }
