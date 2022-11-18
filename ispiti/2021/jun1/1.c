feaae4ba93809c47f579e8197e073936047d66ca
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-18 12:39:01 +0100

    epoha

--- ispiti/2021/jun1/1.c
+++ ispiti/2021/jun1/1.c
@@ -27,9 +27,24 @@ void greska(const char *msg)
     exit(EXIT_FAILURE);
 }
 
+// a.out epoha
 int main(int argc, char **argv)
-{
+{   
+    if (argc != 2)
+        greska("args failed");
 
+    long epoha = atol(argv[1]);
+    struct tm *vreme = localtime(&epoha);
+        if (vreme == NULL)
+            greska("localtime failed");
+
+    long sati = 0;
+    sati += vreme->tm_mday * 24;
+    sati += vreme->tm_hour;
+
+    printf("%d\n", vreme->tm_mday);
+    printf("sati: %d, minuti: %d, sekundi: %d\n", vreme->tm_hour, vreme->tm_min, vreme->tm_sec);
+    printf("%ld\n", sati);
 
 
     exit(EXIT_SUCCESS);
