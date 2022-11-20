f887379a20e5d1f228b845fc20f4e5aca0d514a2
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-11-20 11:01:10 +0100

    signali 10/10

--- webgrade/2019_JAN1B/2.c
+++ webgrade/2019_JAN1B/2.c
@@ -27,8 +27,60 @@ void greska(const char *msg)
     exit(EXIT_FAILURE);
 }
 
-int main(int argc, char **argv)
+int usr1 = 0;
+int usr2 = 0;
+int term = 0;
+int sint = 0;
+
+void signal_heandler(int signum)
 {
+    switch (signum) {
+        case SIGUSR1: usr1 = 1; break;
+        case SIGUSR2: usr2 = 1; break;
+        case SIGINT: sint = 1; break;
+        case SIGTERM: term = 1; break;
+    }
+}
+
+int main()
+{   
+    int score = 0;
+
+    while (true) {
+        if (signal(SIGUSR1, signal_heandler) == SIG_ERR)
+            greska("signal user1 failed");
+
+        if (signal(SIGUSR2, signal_heandler) == SIG_ERR)
+            greska("signal user2 failed");
+
+        if (signal(SIGINT, signal_heandler) == SIG_ERR)
+            greska("signal int failed");
+
+        if (signal(SIGTERM, signal_heandler) == SIG_ERR)
+            greska("signal term failed");
+
+        pause();
+
+        if (term)
+            break;
+
+        if (usr1) {
+            score++;
+            usr1 = 0;
+        }
+
+        if (usr2) {
+            score += 2;
+            usr2 = 0;
+        }
+
+        if (sint) {
+            score -= 4;
+            sint = 0;
+        }
+    }
+
+    printf("%d\n", score);
 
 
 
