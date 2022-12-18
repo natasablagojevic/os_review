b641e73b55fae344ddc03fbe3791302c9f587c5c
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-12-18 09:19:29 +0100

    find . -type f (proccess)

---
+++ webgrade/2017_JUN2/2.c
@@ -0,0 +1,100 @@
+#define _XOPEN_SOURCE 700
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<ftw.h>
+#include<semaphore.h>
+#include<sys/mman.h>
+#include<sys/wait.h>
+#include<pthread.h>
+#include<time.h>
+#include<string.h>
+#include<math.h>
+#include<ctype.h>
+#include<stdbool.h>
+
+#define UNUSED(X) (void)(X)
+#define RD_END 0
+#define WR_END 1
+
+void greska(const char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+// a.out ekstenzija
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        greska("args failed");
+
+    int cld2Par[2];
+    if (pipe(cld2Par) == -1)
+        greska("pipe failed");
+
+    pid_t childPid = fork();
+        if (childPid == -1)
+            greska("fork failed");
+
+    if (childPid > 0) {
+        /* PARENT */
+        close(cld2Par[WR_END]);
+
+        FILE *f = fdopen(cld2Par[RD_END], "r");
+            if (f == NULL)
+                greska("fdopen failed");
+
+        int brojac = 0;
+        char *buf = NULL;
+
+        while (fscanf(f, "%ms", &buf) == 1) {
+            char *dot = strrchr(buf, '.');
+            
+            if (dot == NULL)
+                greska("strrchr failed");
+
+            if (strcmp(dot, argv[1]) == 0)
+                brojac++;
+        }
+
+        printf("%d\n", brojac);
+
+
+        fclose(f);
+        free(buf);
+        close(cld2Par[RD_END]);
+    } else {
+        /* CHILD */
+        close(cld2Par[RD_END]);
+
+        if (dup2(cld2Par[WR_END], STDOUT_FILENO) == -1)
+            greska("dup2 failed");
+
+        if (execlp("find", "find", ".", "-type", "f", NULL) == -1)
+            exit(EXIT_FAILURE);
+
+        close(cld2Par[WR_END]);
+    }
+
+    int status = 0;
+    if (wait(&status) == -1)
+        greska("wait failed");
+
+    if (WIFEXITED(status)) {
+        if (WEXITSTATUS(status) == EXIT_SUCCESS)
+            fprintf(stderr, "uspeh\n");
+        else 
+            fprintf(stderr, "neuspeh\n");
+    } else 
+        fprintf(stderr, "neuspeh\n");
+
+
+    close(cld2Par[RD_END]);
+    close(cld2Par[WR_END]);
+    exit(EXIT_SUCCESS);
+}
