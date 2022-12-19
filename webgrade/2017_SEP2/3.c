fe55dba37a9eaf6dfb9529ab1215375f8d7a7c89
natasablagojevic <blagojevicnatasa360@gmail.com>
2022-12-19 13:51:14 +0100

    10/10

--- webgrade/2017_SEP2/3.c
+++ webgrade/2017_SEP2/3.c
@@ -27,8 +27,78 @@ void greska(const char *msg)
     exit(EXIT_FAILURE);
 }
 
-int main(int argc, char **argv)
+int main()
 {
+    char **command = malloc(3 * sizeof(char *));
+        if (command == NULL)
+            greska("command malloc failed");
+
+    for (int i = 0; i < 3; i++) {
+        command[i] = malloc(256 * sizeof(char));
+            if (command[i] == NULL) {
+                for (int j = 0; j < i; j++)
+                    free(command[j]);
+
+                free(command);
+                greska("command[i] malloc failed");
+            }
+    }
+
+    while (scanf("%s%s%s", command[0], command[1], command[2]) != EOF) {
+        int cld2Par[2];
+        if (pipe(cld2Par) == -1)
+            greska("pipe failed");
+
+        pid_t childPid = fork();
+            if (childPid == -1)
+                greska("fork failed");
+
+        if (childPid == 0) {
+            close(cld2Par[RD_END]);
+
+            if (dup2(cld2Par[WR_END], STDOUT_FILENO) == -1)
+                greska("dup2 failed");
+
+            if (execlp("expr", "expr", command[0], command[2], command[1], NULL) == -1)
+                exit(EXIT_FAILURE);
+
+            close(cld2Par[WR_END]);
+            exit(EXIT_FAILURE);
+        } else {
+            close(cld2Par[WR_END]);
+
+            char *linija = NULL;
+            size_t size = 0;
+
+            FILE *f = fdopen(cld2Par[RD_END], "r");
+                if (f == NULL)
+                    greska("fdopen failed");
+
+            if (getline(&linija, &size, f) == -1)
+                greska("getline failed");
+            
+            // char *buf = NULL;
+            // fscanf(f, "%ms", &buf);
+
+            printf("%s", linija);
+            
+            fclose(f);
+            // free(buf);
+            close(cld2Par[RD_END]);
+        }
+
+        int status = 0;
+        if (wait(&status) == -1)
+            greska("wait failed");
+
+        if (WIFEXITED(status)) {
+            if (WEXITSTATUS(status) == EXIT_SUCCESS)
+                fprintf(stderr, "uspeh\n");
+            else 
+                fprintf(stderr, "neuspeh\n");
+        } else 
+            fprintf(stderr, "neuspeh\n");
+    }
 
 
 
