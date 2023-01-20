8e0219ee915956c0f0626c26e2bbf984ae5623ba
natasablagojevic <blagojevicnatasa360@gmail.com>
2023-01-20 10:59:34 +0100

    testing

--- semafori/1_wr.c
+++ semafori/1_wr.c
@@ -33,19 +33,16 @@ void greska(const char *msg)
     exit(EXIT_FAILURE);
 }
 
-void *get_memory_block(char *path, unsigned *size)
+void *get_memory_block(char *path, unsigned size)
 {
-    int fd = shm_open(path, O_RDWR, 0600);
+    int fd = shm_open(path, O_RDWR | O_CREAT, 0600);
         if (fd == -1)
             greska("shm_open failed");
 
-    struct stat sb;
-    if (fstat(fd, &sb) == -1)
-        greska("fstat failed");
+    if (ftruncate(fd, size) == -1)
+        greska("ftruncate failed");
 
-    *size = sb.st_size;
-
-    void *addr = mmap(NULL, *size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    void *addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
         if (addr == MAP_FAILED)
             greska("mmap failed");
 
@@ -55,7 +52,7 @@ void *get_memory_block(char *path, unsigned *size)
 
 double rastojanje(double x1, double y1, double x2, double y2)
 {
-    return sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+    return (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
 }
 
 // a.out memsh
@@ -64,36 +61,31 @@ int main(int argc, char **argv)
     if (argc != 2)
         greska("args failed");
 
-    unsigned size = 0;
-    OsInputData *p = get_memory_block(argv[1], &size);
+    OsInputData *p = get_memory_block(argv[1], sizeof(OsInputData));
 
     // if (sem_wait(&(p->ready)) == -1)
     //     greska("sem_wait failed");
     
-    if (sem_init(&(p->ready), 1, 1) == -1)
+    if (sem_init(&(p->ready), 1, 0) == -1)
         greska("sem_init failed");
 
-    double min = DBL_MAX;
-    double x = DBL_MAX;
-
-    for (unsigned i = 0; i < p->nPoints; i += 2) {
-        for (unsigned j = 0; i != j && j < p->nPoints; j += 2) {
-            x = rastojanje(p->points[i], p->points[i+1], p->points[j], p->points[j+1]);
-            printf("x: %lf\n", x);
-            if (x < min)
-                min = x;
-
-            printf("\tmin: %lf\n", min);
-        }
-    }
+    p->nPoints = 10;
+    p->points[0] = -1.3;
+    p->points[1] = 1.7;
+    p->points[2] = 15.4;
+    p->points[3] = -101.3;
+    p->points[4] = -1.4;
+    p->points[5] = 1.9;
+    p->points[6] = 13.2;
+    p->points[7] = -99.5;
+    p->points[8] = -17;
+    p->points[9] = 0;
 
     if (sem_post(&(p->done)) == -1)
         greska("sem_post failed");
 
-    if (munmap(p, size) == -1)
+    if (munmap(p, sizeof(OsInputData)) == -1)
         greska("munmap failed");
-
-    printf("%lf\n", min);
 
     // if (shm_unlink(argv[1]) == -1)
     //     greska("shm_unlink failed");
